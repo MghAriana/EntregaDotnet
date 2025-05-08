2c9275f11ba784cdea11b35d4d1972c8ca1c095a
MghAriana <Arianamarilla8@gmail.com>
2025-05-08 10:51:06 -0300

    first commit

---
+++ CentroEventos.Aplicacion/Persona.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace CentroEventos.Aplicacion;
+
+public class Persona{
+    
+}
---
+++ CentroEventos.Repositorios/RepositorioReservaTXT.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CentroEventos.Repositorios;
+
+public class RepositorioReservaTXT
+{
+
+}// Consultar como persistir el ID en particular si es correcto almacenar el valor del ultimo (ID) en un txt y asignarlo a la variable id de persona 
