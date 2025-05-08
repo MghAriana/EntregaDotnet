104cf94f200d655e3a7130f74ece8a641075b3ce
Sheila <spisco@fi.uba.ar>
2025-05-08 13:13:14 -0300

    Eliminación de  2 Clase1.cs que fueron creadas automáticamente. - Modificaciones en Interfaz IRepositorioEventoDeportivo: 3 metodos nuevos. - Modificaciones en RepositorioEventoDeportivoTXT: Implementación de los tres métodos de IRepositorioEventoDeportivo

--- CentroEventos.Repositorios/RepositorioEventoDeportivoTXT.cs
+++ CentroEventos.Repositorios/RepositorioEventoDeportivoTXT.cs
@@ -1,8 +1,27 @@
 using System;
 
 namespace CentroEventos.Repositorios;
+using CentroEventos.Aplicacion;
 
-public class RepositorioEventoDeportivoTXT
+public class RepositorioEventoDeportivoTXT: IRepositorioEventoDeportivo
 {
+    readonly string _archivo = "eventos_deportivos.txt";
+    public void AgregarEvento(EventoDeportivo edep)
+    {
+        //implementar
+    }
 
+    public bool ExistePersona(int responsableId)
+    {
+        //implementar
+        return false;
+    }
+
+    public List<EventoDeportivo> ListarEventosConCupo(){
+
+        List<EventoDeportivo> lista = new List<EventoDeportivo>();
+        //implementar
+        return lista;
+
+    }
 }
