48abbfbb8a59e91e758a5620dc68a73aa5b1cb70
Sheila <spisco@fi.uba.ar>
2025-05-22 19:10:40 -0300

    Ultimas modificaciones previas a la entrega

--- CentroEventos.Aplicacion/EventoDeportivoBajaUseCase.cs
+++ CentroEventos.Aplicacion/EventoDeportivoBajaUseCase.cs
@@ -7,8 +7,9 @@ namespace CentroEventos.Aplicacion;
 public class EventoDeportivoBajaUseCase(IRepositorioEventoDeportivo repoE,IRepositorioReserva repoR) //inyección de dependencias
 {
     public void Ejecutar(int id_evento)
-    {   
-        if(repoR.existenReservasAsociadasAlEvento(id_evento))
+    {
+        Console.WriteLine(id_evento);
+        if(!repoR.existenReservasAsociadasAlEvento(id_evento))
         {
             throw new Exception("No se puede eliminar el evento porque existen reservas asociadas.");
         }
